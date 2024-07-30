462973e8390d8fe5418fc7a4cb5fee00724a7279
martineric3694 <martineric3694@gmail.com>
2024-07-30 08:26:40 +0700

    SP Postgres

---
+++ 14-PostgreSQL/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	// Konfigurasi koneksi database
+	connStr := "user=postgres dbname=postgre password=postgres host=127.0.0.1 port=5432 sslmode=disable"
+
+	// Membuka koneksi ke database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Memeriksa koneksi
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Koneksi ke database berhasil!")
+
+	nama := "adi"
+	umur := 16
+
+	// Memanggil stored procedure
+	// Cara buat procedure insert di postgres
+	/*
+		CREATE OR REPLACE PROCEDURE public.masukan_data(IN nama_orang character varying, IN umur_orang numeric)
+		LANGUAGE plpgsql
+		AS $procedure$
+		BEGIN
+			INSERT INTO data(name,umur) VALUES (nama_orang,umur_orang);
+		END;
+		$procedure$
+	*/
+	_, err = db.Exec("CALL masukan_data($1, $2)", nama, umur)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Stored procedure berhasil dipanggil!")
+
+	// Memanggil stored procedure dan menangani hasilnya
+	// Cara buat procedure select postgres
+	/*
+		CREATE OR REPLACE FUNCTION public.kembali_data()
+		RETURNS TABLE(id_data integer, nama character varying, umur_data numeric)
+		LANGUAGE plpgsql
+		AS $function$
+		BEGIN
+			RETURN QUERY SELECT id, name, umur FROM data;
+		END;
+		$function$
+	*/
+
+	rows, err := db.Query("SELECT id_data,nama,umur_data FROM kembali_data()")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var result []map[string]interface{}
+	for rows.Next() {
+		var id int
+		var nama string
+		var umur float64
+
+		data := map[string]interface{}{}
+
+		err := rows.Scan(&id, &nama, &umur)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data["id"] = id
+		data["nama"] = nama
+		data["umur"] = umur
+		result = append(result, data)
+
+	}
+
+	fmt.Println(result)
+}
